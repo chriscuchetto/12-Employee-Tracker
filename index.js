7084594c2c7a3e2b7dddd2289c46a2155949af97
chriscuchetto <chriscuchetto@gmail.com>
2023-01-25 23:41:48 -0600

    made index.js file and installed necessary modules

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: process.env.PW,
+    database: "employee_db",
+  },
+  console.log(`Connected to the tracker database.`)
+);
+
+function viewDepartments() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+  });
+}
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        { name: "View all departments", value: "VIEW DEPARTMENTS" },
+        { name: "View all roles", value: "VIEW ROLES" },
+      ],
+    },
+  ])
+  .then((response) => {
+    if (response.choice === "VIEW DEPARTMENTS") {
+      viewDepartments();
+    }
+  });
