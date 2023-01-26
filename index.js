621536d1239ef3ad80272de46ab32d4737573569
chriscuchetto <chriscuchetto@gmail.com>
2023-01-26 02:06:24 -0600

    added a add department and EXIT option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+require("dotenv").config();
 
 const db = mysql.createConnection(
   {
@@ -8,20 +9,66 @@ const db = mysql.createConnection(
     // MySQL username,
     user: "root",
     // MySQL password
-    password:"PutYourPasswordHere",
+    password: process.env.PW,
     database: "employee_db",
   },
   console.log(`Connected to the employee_db database.`)
 );
 
+
 function viewDepartments() {
-  db.query("SELECT * FROM department", function (err, results) {
+    db.query("SELECT * FROM department", function (err, results) {
+        console.table(results);
+    });
+}
+
+function viewRoles() {
+    db.query("SELECT * FROM role", function (err, results) {
     console.table(results);
   });
 }
 
-inquirer
-  .prompt([
+function viewEmployees() {
+    db.query("SELECT * FROM employee", function (err, results) {
+      console.table(results);
+    });
+  }
+
+  function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of department?",
+            name: "name",
+        },
+    ])
+    .then((answer) => {
+
+        db.query("INSERT INTO department SET ?", answer, function (err, results) {
+            console.table(results);
+            console.log("New Department has been added!");
+        });
+    });
+  }
+  function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of department?",
+            name: "name",
+        },
+    ])
+    .then((answer) => {
+
+        db.query("INSERT INTO department SET ?", answer, function (err, results) {
+            console.table(results);
+            console.log("New Department has been added!");
+        });
+    });
+  }
+
+  function makeAnother() {
+inquirer.prompt([
     {
       type: "list",
       message: "What would you like to do?",
@@ -29,11 +76,32 @@ inquirer
       choices: [
         { name: "View all departments", value: "VIEW DEPARTMENTS" },
         { name: "View all roles", value: "VIEW ROLES" },
+        { name: "View all employees", value: "VIEW EMPLOYEES" },
+        { name: "Add a department", value: "ADD DEPARTMENT" },
+        { name: "EXIT?", value: "EXIT" },
+
       ],
     },
   ])
   .then((response) => {
     if (response.choice === "VIEW DEPARTMENTS") {
-      viewDepartments();
+        viewDepartments();
     }
+    if (response.choice === "VIEW ROLES") {
+        viewRoles();
+      }
+    if (response.choice === "VIEW EMPLOYEES") {
+        viewEmployees();
+      }
+    if (response.choice === "ADD DEPARTMENT") {
+        addDepartment();
+      }
+    if (response.choice === "ADD ROLE") {
+        addRole();
+      }
+    if (response.choice === "EXIT") {
+        process.exit();
+      }  
   });
+}
+makeAnother();
